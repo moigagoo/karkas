862fb46d0a35fc5b4d0121f3667ab854bd031ee9
Constantine Molchanov <moigagoo@live.com>
2023-05-24 15:54:18 +0400

    Demo: Showcase notifications.

--- demo/src/demo/pages/index.nim
+++ demo/src/demo/pages/index.nim
@@ -1,13 +1,16 @@
+import std/strutils
+
 import karax/[karax, kbase, karaxdsl, kdom, vdom]
 import kraut/context
+import karkas/[vbox, hbox, box]
 
 import ../[pages, state, layout]
 import ../components/notification
 
 
-
 var
   entryText = ""
+  nkind = NotificationKind.info
 
 
 proc render*(context: Context): VNode =
@@ -17,26 +20,45 @@ proc render*(context: Context): VNode =
   layout.render buildHtml(tdiv) do:
     h1: text "index"
 
-    textarea:
-      proc onKeyUp(event: Event, target: VNode) =
-        entryText = $target.value
+    let
+      form = HBox()
+      leftSide = VBox()
+      rightSide = VBox()
+      textareaBox = Box()
+      buttonBox = Box()
 
-    button:
-      text "Press me"
-      proc onClick(event: Event, target: VNode) =
-        let
-          contentWrapper = buildHtml(tdiv):
-            p:
-              text kstring entryText
-        var
-          n = Notification(nkind: NotificationKind.info, title: "Click to close", contentWrapper: contentWrapper)
+    form.render buildHtml(tdiv) do:
+      leftSide.render buildHtml(tdiv) do:
+        textareaBox.render buildHtml(tdiv) do:
+          textarea(autofocus = "1"):
+            proc onKeyUp(event: Event, target: VNode) =
+              entryText = $target.value
+        buttonBox.render buildHtml(tdiv) do:
+          button:
+            text "Press me"
+            proc onClick(event: Event, target: VNode) =
+              let
+                contentWrapper = buildHtml(tdiv):
+                  p:
+                    text kstring entryText
+              var
+                n = Notification(nkind: nkind, title: "Click to close", contentWrapper: contentWrapper)
 
-        n.events = @{
-          onclick: proc(event: Event, target: VNode) {.closure.} =
-            let i = state.notifications.find(n)
-            state.notifications.delete(i)
-            redraw()
-        }
+              n.events = @{
+                onclick: proc(event: Event, target: VNode) {.closure.} =
+                  let i = state.notifications.find(n)
+                  state.notifications.delete(i)
+                  redraw()
+              }
 
-        state.notifications.add n
+              state.notifications.add n
+      rightSide.render buildHtml(tdiv) do:
+        for k in NotificationKind:
+          let hb = HBox()
+          hb.render buildHtml(tdiv) do:
+            input(name = kstring"nkind", value = kstring $k, `type` = kstring"radio", id = kstring $k):
+              proc onChange(event: Event, target: VNode) =
+                nkind = parseEnum[NotificationKind]($target.value)
+            label(`for` = kstring $k):
+              text kstring $k
 
