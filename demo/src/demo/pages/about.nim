8347d3c49a852e1a0d60f276f6f41a88ac447107
Constantine Molchanov <moigagoo@live.com>
2023-06-05 00:18:52 +0400

    Weird commit.

--- demo/src/demo/pages/about.nim
+++ demo/src/demo/pages/about.nim
@@ -18,14 +18,14 @@ proc render*(context: Context): VNode =
         var n = new Notification
 
         n.title = "About page opened"
-        n.contentWrapper = buildHtml(tdiv):
+        n.contentwrapper = buildhtml(tdiv):
           p:
-            text kstring"This is something"
+            text kstring"this is something"
             bold:
               text kstring"bold"
             text kstring"."
           p:
-            text kstring"This is a "
+            text kstring"this is a "
             a(href = kstring"#/"):
               text "link"
             text kstring"."
@@ -34,7 +34,7 @@ proc render*(context: Context): VNode =
 
     for _ in 1..5:
       p:
-        text kstring"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quid est igitur, cur ita semper deum appellet Epicurus beatum et aeternum? Quaesita enim virtus est, non quae relinqueret naturam, sed quae tueretur. Duo Reges: constructio interrete. Illa argumenta propria videamus, cur omnia sint paria peccata. Qui est in parvis malis. Qui ita affectus, beatum esse numquam probabis; Dicimus aliquem hilare vivere; Illis videtur, qui illud non dubitant bonum dicere -;"
+        text kstring"lorem ipsum dolor sit amet, consectetur adipiscing elit. Quid est igitur, cur ita semper deum appellet Epicurus beatum et aeternum? Quaesita enim virtus est, non quae relinqueret naturam, sed quae tueretur. Duo Reges: constructio interrete. Illa argumenta propria videamus, cur omnia sint paria peccata. Qui est in parvis malis. Qui ita affectus, beatum esse numquam probabis; Dicimus aliquem hilare vivere; Illis videtur, qui illud non dubitant bonum dicere -;"
       p:
         text kstring"Prodest, inquit, mihi eo esse animo. Nam his libris eum malo quam reliquo ornatu villae delectari. Sed tamen enitar et, si minus multa mihi occurrent, non fugiam ista popularia. Cur iustitia laudatur? Quid de Pythagora? Levatio igitur vitiorum magna fit in iis, qui habent ad virtutem progressionis aliquantum. Et hercule-fatendum est enim, quod sentio -mirabilis est apud illos contextus rerum. Recte, inquit, intellegis. Primum in nostrane potestate est, quid meminerimus?"
       p:
